f12233f3b9e2acfebe1086edbea31b31ce8e5102
Howtoautomate.in.th <mart@howtoautomate.in.th>
2020-07-07 15:04:11 +0700

    Simple usage of json temaplte engine

---
+++ index.js
@@ -0,0 +1,46 @@
+const template = {
+    foo: "bar",
+    "#comment": "put comment",
+    foo2: {
+        "#if": {
+            condition: "{{foo2.enable}}",
+            then: { data: "foo2-then" },
+            else: { data: "foo2-else" }
+        }
+    },
+    foos: {
+        "#each": {
+            values: "#range(3, 0)", // or [0, 1, 2]
+            iteration: "foo-{{iteration.value}}-{{iteration.index}}"
+        }
+    },
+    excludeField: {
+        notParsed: "{{qwe}}"
+    }
+};
+const data = {
+    foo2: {
+        enable: false
+    }
+};
+
+
+async function compileJsonTemplate(template, data, options) {
+    return await JsonParser.compile(template, data, options);
+}
+
+async function compileData() {
+    var jsonObject = await compileJsonTemplate(template, data, options)
+    console.log(jsonObject);
+}
+
+const JSONTemplateEngine = require("json-template-engine");
+const JsonParser = new JSONTemplateEngine();
+
+const options = {
+    helpers: true, // parse helpers? default true
+    values: true, // parse values? default true
+    exclude: ["excludeField"] // which fields in the object should be excluded (fields will not be parsed), default []
+};
+
+compileData();
